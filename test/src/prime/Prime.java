bc9482a4aab00626709f540c51db75842093d28a
Java-Point <45845372+Java-Point@users.noreply.github.com>
2019-10-10 15:40:50 +0530

    Add files via upload

---
+++ test/src/prime/Prime.java
@@ -0,0 +1,38 @@
+package prime;
+
+import java.util.function.IntPredicate;
+import java.util.stream.IntStream;
+
+public class Prime {
+
+	public static void main(String[] args) {
+
+		System.out.println("*****Java7*****");
+		for (int i = 1; i < 100; i++) {
+
+			boolean isPrime = true;
+
+			for (int j = 2; j < i; j++) {
+
+				if (i % j == 0) {
+					isPrime = false;
+				}
+			}
+			// print the number
+			if (isPrime == true) {
+				System.out.print(i + " ");
+			}
+		}
+		System.out.println("*****Java8*****");
+		final IntPredicate isPrime = n -> {
+			for (int i = 2; i < n; i++) {
+				if (n % i == 0) {
+					return false;
+				}
+			}
+			return true;
+		};
+		System.out.println("*****Java8*****");
+		IntStream.range(1, 100).filter(isPrime).forEach(System.out::println);
+	}
+}
