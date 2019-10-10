0dd6bc76c4d4af0257ad35f10ea1ce7adddfd739
Java-Point <45845372+Java-Point@users.noreply.github.com>
2019-10-10 16:00:22 +0530

    Update Even.java

--- test/src/even/number/Even.java
+++ test/src/even/number/Even.java
@@ -14,5 +14,11 @@ public class Even {
 			} else
 				System.out.println("The no is even");
 		}
+		
+		//OR
+		List<Integer> listOfIntegers = Arrays.asList(1, 2, 3, 4, 56, 7, 89, 10);
+		System.out.println("All values: " + listOfIntegers);
+		Integer max = listOfIntegers.stream().mapToInt(v -> v).max().orElseThrow(NoSuchElementException::new);
+		System.out.println("MAX: " + max);
 	}
 }
