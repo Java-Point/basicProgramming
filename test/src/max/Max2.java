533cece37fa81edb9477020521dfacb5f145e78a
Java-Point <45845372+Java-Point@users.noreply.github.com>
2019-10-11 11:29:48 +0530

    Update Max2.java

--- test/src/max/Max2.java
+++ test/src/max/Max2.java
@@ -4,6 +4,7 @@ public class Max2 {
 
 	public static void main(String[] args) {
 		int[] x = { 12, 23, 432, 2342, 43 };
+		Arrays.sort(x);
 		int x1 = x[0], x2 = x[0];
 		for (int i = 1; i < x.length; i++) {
 			if (x[i] > x1) {
