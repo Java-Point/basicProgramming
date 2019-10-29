03bfc59240e5542cd0aa6d6e5b71b87544e90187
Java-Point <45845372+Java-Point@users.noreply.github.com>
2019-10-29 15:24:19 +0530

    Add files via upload

---
+++ test/src/traverse/TraverseWithoutReverse.java
@@ -0,0 +1,12 @@
+package test;
+
+public class TraverseWithoutReverse {
+
+	public static void main(String[] args) {
+		String s = "Rakesh";
+		char[] ch = s.toCharArray();
+		for (int i = s.length() - 1; i >= 0; i--) {
+			System.out.print(ch[i]);
+		}
+	}
+}
