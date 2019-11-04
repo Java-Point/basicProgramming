ae55be4ef46b1dcd4aa77b94bf0fa03908206f5a
Java-Point <45845372+Java-Point@users.noreply.github.com>
2019-11-04 14:50:05 +0530

    Update Prime.java

--- test/src/prime/Prime.java
+++ test/src/prime/Prime.java
@@ -8,11 +8,11 @@ public class Prime {
 	public static void main(String[] args) {
 
 		System.out.println("*****Java7*****");
-		for (int i = 1; i < 100; i++) {
+		for (int i = 2; i <= 100; i++) {
 
 			boolean isPrime = true;
 
-			for (int j = 2; j < i; j++) {
+			for (int j = 2; j <= i / 2; j++) {
 
 				if (i % j == 0) {
 					isPrime = false;
